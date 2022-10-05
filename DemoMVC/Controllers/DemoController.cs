7f96773ceb898f246846fd43e0d77b226df5ad5a
minhcoi113 <minaphamtan@gmail.com>
2022-10-05 15:53:38 +0700

    first commit

---
+++ DemoMVC/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Test1()
+        {
+            string hello = " hello class dhcntt19a....";
+            return hello;
+        }
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
